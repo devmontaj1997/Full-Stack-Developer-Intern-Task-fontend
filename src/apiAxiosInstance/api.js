7d13a897d1c4cb95782ca267b153404152e406ba
devmontaj1997 <sekhmontajulali1997@gmail.com>
2024-10-30 09:07:05 +0530

    connect to backend

--- src/apiAxiosInstance/api.js
+++ src/apiAxiosInstance/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5050/api/v1/exercise",
+  baseURL: "https://fullstackdeveloperinterntask-backend.onrender.com/api/v1/exercise",
   timeout: 20000,
   withCredentials: true,
 });
